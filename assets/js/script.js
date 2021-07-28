3b9951d718a32c5b851a6588358bbf21e7d675ed
Shital Chaudhary <shitalchau10@gmail.com>
2021-07-27 21:09:46 -0500

    add individual time blocks and get input data

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,41 +15,53 @@ var newFormat = {
 
 currentDay.innerHTML = now.toLocaleString(newFormat);
 
-var scheudle = [{
-    time: '8:00AM',
-    description: '',
-    hour: 8
-}, ];
 
-var displayTimeblocks = function() {
-    // for(var i = 0; i < timeblocks.length; i++ ){
-    // select entire container
-    var containerEL = document.querySelector(".container");
-    // create a div with a call name of 'time-block'
-    var divEl = document.createElement("div");
-    divEl.className = "individual-timeblock";
+// var displayTimeblocks = function() {
+// //     // for(var i = 0; i < timeblocks.length; i++ ){
+// //     // select entire container
+// //     var containerEL = document.querySelector(".container");
+// //     // create a div with a call name of 'time-block'
+// //     var divEl = document.createElement("div");
+// //     divEl.className = "individual-timeblock";
 
-    // create a span that shows the time from 9am to 5pm 
-    var timeEl = document.createElement("span");
-    timeEl.className = "time-span";
-    timeEl.textContent = "Test";
-    divEl.appendChild(timeEl);
+// //     // create a span that shows the time from 9am to 5pm 
+// //     var timeEl = document.createElement("span");
+// //     timeEl.className = "time-span";
+// //     timeEl.textContent = "Test";
+// //     divEl.appendChild(timeEl);
 
-    // create a text field that users can write on 
-    var inputEl = document.createElement("input");
-    inputEl.className = "input";
-    divEl.appendChild(inputEl);
+// //     // create a text field that users can write on 
+// //     var inputEl = document.createElement("input");
+// //     inputEl.className = "input";
+// //     divEl.appendChild(inputEl);
 
-    // create button to save to localstorage
-    var saveBtnEl = document.createElement("button");
-    saveBtnEl.className = "saveBtn";
-    saveBtnEl.setAttribute('type', 'submit')
-    saveBtnEl.innerText = "Submit";
-    divEl.appendChild(saveBtnEl);
+// //     // create button to save to localstorage
+// //     var saveBtnEl = document.createElement("button");
+// //     saveBtnEl.className = "saveBtn";
+// //     saveBtnEl.setAttribute('type', 'submit')
+// //     saveBtnEl.innerText = "Submit";
+// //     divEl.appendChild(saveBtnEl);
 
-    containerEL.appendChild(divEl);
+// //     containerEL.appendChild(divEl);
+// // // }
 // }
+
+// displayTimeblocks();
+
+var saveBtn = document.getElementsByClassName("saveBtn");
+
+console.log(saveBtn);
+var savedTask = function(e){
+    var time = e.target.getAttribute("data-hours");
+    console.log(time);
+
+    var tasks = document.getElementById(time).value;
+    console.log(tasks);
+}
+var saveData = function(){
+    
 }
 
-displayTimeblocks();
-
+for (const button of saveBtn) {
+    button.addEventListener('click', savedTask);
+}
